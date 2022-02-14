f2fff3a0de8b119f83e7ad3afb2504b346d71f3c
camsosa2021 <camsosa2021@icloud.com>
2022-02-13 18:33:39 -0600

    tried to fix

--- nodes.py
+++ nodes.py
@@ -13,32 +13,32 @@ class Node:
         self.next_node = next_node
 
 class LinkedList:
-  def __init__(self, value=None):
-    self.head_node = Node(value)
+    def __init__(self, value=None):
+     self.head_node = Node(value)
   
-  def get_head_node(self):
-    return self.head_node
+    def get_head_node(self):
+     return self.head_node
   
-  def insert_beginning(self, new_value):
-    new_node = Node(new_value)
-    new_node.set_next_node(self.head_node)
-    self.head_node = new_node
+    def insert_beginning(self, new_value):
+      new_node = Node(new_value)
+      new_node.set_next_node(self.head_node)
+      self.head_node = new_node
     
-  def stringify_list(self):
-    string_list = ""
-    current_node = self.get_head_node()
-    while current_node:
-      if current_node.get_value() != None:
+    def stringify_list(self):
+      string_list = ""
+      current_node = self.get_head_node()
+      while current_node:
+       if current_node.get_value() != None:
         string_list += str(current_node.get_value()) + "\n"
       current_node = current_node.get_next_node()
-    return string_list
+      return string_list
   
-  def remove_node(self, value_to_remove):
-    current_node = self.get_head_node()
-    if current_node.get_value() == value_to_remove:
-      self.head_node = current_node.get_next_node()
-    else:
-      while current_node:
+    def remove_node(self, value_to_remove):
+      current_node = self.get_head_node()
+      if current_node.get_value() == value_to_remove:
+       self.head_node = current_node.get_next_node()
+      else:
+       while current_node:
         next_node = current_node.get_next_node()
         if next_node.get_value() == value_to_remove:
           current_node.set_next_node(next_node.get_next_node())
@@ -46,7 +46,7 @@ class LinkedList:
         else:
           current_node = next_node
 
-  def swap_nodes(input_list, val1, val2):
+    def swap_nodes(input_list, val1, val2):
       print(f'Swapping {val1} with {val2}')
       node1_prev = None
       node2_prev = None
